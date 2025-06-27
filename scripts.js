05c3a6e513ddea412b4ca9917292a4d3345c9669
Saksham Setia <setiasaksham15@gmail.com>
2025-06-27 13:16:35 +0530

    Added Tab Change Function

--- scripts.js
+++ scripts.js
@@ -11,3 +11,34 @@ function clock(){
 }
 
 setInterval(clock,1000);
+
+//Tab Switch Code
+let tabArr = document.querySelectorAll('.tab');
+let navArr = document.querySelectorAll('.navigate')
+
+function switchTab(tab,target){
+    console.log(target);
+    tabArr.forEach(item=>{
+        item.classList.add('hidden');
+    })
+    tab.classList.remove('hidden');
+    navArr.forEach(item=>{
+        item.classList.remove('glow');
+    })
+    target.classList.add('glow');
+}
+document.getElementById('DashBoard-bu').addEventListener('click',(event) =>{
+    switchTab(document.getElementById('DashBoard'),event.target);
+})
+document.getElementById('Tasks-bu').addEventListener('click',(event) =>{
+    switchTab(document.getElementById('Tasks'),event.target);
+})
+document.getElementById('Timer-bu').addEventListener('click',(event) =>{
+    switchTab(document.getElementById('Timer'),event.target);
+})
+document.getElementById('Notes-bu').addEventListener('click',(event) =>{
+    switchTab(document.getElementById('Notes'),event.target);
+})
+document.getElementById('Play-bu').addEventListener('click',(event) =>{
+    switchTab(document.getElementById('Play'),event.target);
+})
